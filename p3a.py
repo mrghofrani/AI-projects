2d2e05a528745e7acb49212d6a988f3ac9d5eab8
Mohammadreza Ghofrani <mohammadrezaghofrany@gmail.com>
2019-12-23 22:37:26 +0330

    goal_test funciton implemented

--- p3a.py
+++ p3a.py
@@ -1,3 +1,17 @@
+cube = list()
+
+
+def goal_test(node):
+    if cube[0] == [1,1,1,1] and \
+        cube[1] == [2,2,2,2] and \
+        cube[2] == [3,3,3,3] and \
+        cube[3] == [4,4,4,4] and \
+        cube[4] == [5,5,5,5] and \
+        cube[5] == [6,6,6,6]:
+        return True
+    return False
+
+
 def depth_limited_search_decorator(initial_node):
     limit = 0
     while True:
