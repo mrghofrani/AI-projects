f15deea5d92f3722f76a85db5e9f15e44fb3448e
Mohammadreza Ghofrani <mohammadrezaghofrany@gmial.com>
2020-01-17 02:26:59 +0330

    Added a unittest file and consistent function has been tested

--- number_assigner.py
+++ number_assigner.py
@@ -20,6 +20,8 @@ def select_node(assignment, domain, mode):
 
 def consistent(assignment, node, node_type, edge):
     ntype = node_type[node]
+    if ntype == 'C':
+        return True # There is no consistent checking for circle
     neighbour_value = 1 if ntype == 'T' or ntype == 'S' else 0
     valid = False
     for n in edge[node]:
@@ -31,21 +33,23 @@ def consistent(assignment, node, node_type, edge):
         elif ntype == 'P' or ntype == 'H':
             neighbour_value += assignment[n]
     if valid:
-        if ntype == 'T' or ntype == 'P':
-            return assignment[node] != neighbour_value % 10
-        elif ntype == 'S' or ntype == 'H':
-            return assignment[node] != 10**floor(log10(neighbour_value))
+        if ntype == 'S' or ntype == 'H':
+            return assignment[node] == neighbour_value % 10
+        elif ntype == 'T' or ntype == 'P':
+            x = neighbour_value // 10**floor(log10(neighbour_value))
+            return assignment[node] == neighbour_value // 10**floor(log10(neighbour_value))
     return True
 
 
-def valid(assignment, node_type, node_num, edge):
+def valid(assignment, node_type, edge):
     # At first we shuold check that each node has assigned with a value
-    for n in range(node_num):
-        if assignment[n] is None:
+
+    for i,_ in enumerate(node_type):
+        if assignment[i] is None:
             return False
     # And then we should check that each assignment is consistent or not
-    for n in range(node_num):
-        if not consistent(assignment, n, node_type, edge):
+    for i,_ in enumerate(node_type):
+        if not consistent(assignment, i, node_type, edge):
             return False
     return True
 
@@ -132,8 +136,8 @@ def forward_check(assignment, domain, node, node_type, edge):
     return
 
 
-def backtrack(assignment, node_type, domain, node_num, edge, mode):
-    if valid(assignment, node_type, node_num, edge): 
+def backtrack(assignment, node_type, domain, edge, mode):
+    if valid(assignment, node_type, edge): 
         return assignment
     n = select_node(assignment, domain, mode=mode[0])
     for val in domain[n]:
@@ -143,7 +147,7 @@ def backtrack(assignment, node_type, domain, node_num, edge, mode):
             assignment[n] = val
             if mode[1] == '1':
                 forward_check(assignment, domain, node=n, node_type=node_type, edge=edge)
-            result = backtrack(assignment[:], node_type, domain[:], node_num, edge, mode)
+            result = backtrack(assignment[:], node_type, domain[:], edge, mode)
             if result:
                 return result
     return None
@@ -163,25 +167,25 @@ def initialize_list(node_num):
 
 def main():
     mode = "110"
-    node_num = int(input())
-    assignment = [None] * node_num
+    number_of_nodes = int(input())
+    assignment = [None] * number_of_nodes
 
     edge_num = int(input())
 
     raw_node_type = input()
     node_type = raw_node_type.split(' ')
 
-    edge = initialize_list(node_num)
+    edge = initialize_list(number_of_nodes)
     for _ in range(edge_num):
         e = list(map(int,input().split(' ')))
         edge[e[0]].append(e[1])
         edge[e[1]].append(e[0])
 
     domain = []
-    for i in range(node_num):
+    for i in range(number_of_nodes):
         domain.append(list(range(1,10))) # Each node domain could be from 1 up to 9
 
-    solution = backtrack(assignment[:], node_type, domain, node_num, edge, mode)
+    solution = backtrack(assignment[:], node_type, domain, edge, mode)
     if solution is not None:
         print(solution)
 
